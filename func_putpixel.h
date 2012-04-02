f8dc640d317a42130fc87f66975a4ed441835ff4
Takeutch Kemeco <takeutchkemeco@gmail.com>
2012-04-03 06:58:56 +0900

    画面に点を打つ関数を追加
    putpixel(X座標、Y座標、色相、彩度、明度)
    
    備考：
    色相は、２πを３６０度と考えた場合の色角度
    彩度、明度は、１を１００％と考えた数値
    
    スクリーン座標は1920*1080 32bit で決め打ちしてます。
    他のディスプレイ環境だと描画が崩れるけど、持ってないので知りません

---
+++ func_putpixel.h
@@ -0,0 +1,8 @@
+#ifndef __FUNC_PUTPIXEL_H__
+#define __FUNC_PUTPIXEL_H__
+
+void __func_putcolor(double x, double y,
+                     double H, double S, double B);
+void __init_func_putcolor(void);
+
+#endif /* __FUNC_PUTPIXEL_H__ */
