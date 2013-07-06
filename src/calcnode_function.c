5d90a59d95354a444ec7e84ff36a1ef7093d2ee7
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-07-07 05:51:14 +0900

    calcnode_function()を追加
    副作用のみを目的とした関数では、戻り値を返さない場合が多いので困る。
    （putcharやputpixelなどの類）
    戻り値が存在しないことは、それはすなわち０とは異なるので、扱いにこまる。
    Haskellではそれを_|_（ボトム）と解釈して扱っている。
    それに習って同様に、戻り値の無い関数を扱う場合は、その結果のCalcNodeのtypeを
    CNT_BOTTOMとしすることとした。
    
    expression では、引数に非ボトムを期待しているので、CNT_BOTTOMであった場合には矛盾が生じる。
    その場合のexpressionにおける条件分岐（おそらくエラー表示して終了だろう）はまだ実装していない。
    忘れないようにメモ。

---
+++ src/calcnode_function.c
@@ -0,0 +1,89 @@
+/* calcnode_function.c
+ * Copyright (C) 2013 Takeutch Kemeco
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "complex.h"
+#include "node.h"
+#include "calcnode.h"
+#include "calcnode_expression.h"
+#include "calcnode_function.h"
+#include "func_putpixel.h"
+#include "memcalc.bison.h"
+
+static struct CalcNode calcnode_function__FUNC_PRINT(struct Node* a)
+{
+        struct CalcNode cn0 = calcnode_expression(node_child(a, 0));
+
+        const double re = complex_realpart(cn0.compval);
+        const double im = complex_imagpart(cn0.compval);
+
+        if (im >= 0.0)
+                printf("%f+%fi\n", re, im);
+        else
+                printf("%f%fi\n", re, im);
+
+        cn0.type = CNT_BOTTOM;
+
+        return cn0;
+}
+
+static struct CalcNode calcnode_function__FUNC_PUTPIXEL(struct Node* a)
+{
+        struct CalcNode cn0 = calcnode_expression(node_child(a, 0));
+        struct CalcNode cn1 = calcnode_expression(node_child(a, 1));
+        struct CalcNode cn2 = calcnode_expression(node_child(a, 2));
+        struct CalcNode cn3 = calcnode_expression(node_child(a, 3));
+        struct CalcNode cn4 = calcnode_expression(node_child(a, 4));
+
+        const double cn0re = complex_realpart(cn0.compval);
+        const double cn1re = complex_realpart(cn1.compval);
+        const double cn2re = complex_realpart(cn2.compval);
+        const double cn3re = complex_realpart(cn3.compval);
+        const double cn4re = complex_realpart(cn4.compval);
+
+        __func_putpixel(cn0re, cn1re, cn2re, cn3re, cn4re);
+
+        cn0.type = CNT_BOTTOM;
+
+        return cn0;
+}
+
+static struct CalcNode calcnode_function__FUNC_PUTCHAR(struct Node* a)
+{
+        struct CalcNode cn0 = calcnode_expression(node_child(a, 0));
+
+        putchar(complex_realpart(cn0.compval));
+
+        cn0.type = CNT_BOTTOM;
+
+        return cn0;
+}
+
+struct CalcNode calcnode_function(struct Node* a)
+{
+        switch (a->ope) {
+        case __FUNC_PRINT:           return calcnode_function__FUNC_PRINT(a);
+        case __FUNC_PUTPIXEL:        return calcnode_function__FUNC_PUTPIXEL(a);
+        case __FUNC_PUTCHAR:         return calcnode_function__FUNC_PUTCHAR(a);
+        }
+
+        struct CalcNode cn = {.type = CNT_EMPTY};
+        return cn;
+}
---
+++ src/test/test_calcnode_function.c
@@ -0,0 +1,90 @@
+/* test_calcnode_function.c
+ * Copyright (C) 2013 Takeutch Kemeco
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <blike.h>
+#include <stdint.h>
+#include "complex.h"
+#include "node.h"
+#include "calcnode.h"
+#include "calcnode_function.h"
+#include "memcalc.bison.h"
+
+struct Node* create_print(void)
+{
+        struct Node* f0 = node_new(__FUNC_PRINT);
+
+        struct Complex* v0 = complex_new(123, 0);
+        struct Node* n0 = node_new_leaf(__CONST_FLOAT, v0);
+
+        node_link(f0, n0);
+
+        return f0;
+}
+
+struct Node* create_putchar(void)
+{
+        struct Node* f0 = node_new(__FUNC_PUTCHAR);
+
+        struct Complex* v0 = complex_new('A', 0);
+        struct Node* n0 = node_new_leaf(__CONST_FLOAT, v0);
+
+        node_link(f0, n0);
+
+        return f0;
+}
+
+struct Node* create_putpixel(void)
+{
+        struct Node* f0 = node_new(__FUNC_PUTPIXEL);
+
+        struct Complex* v0 = complex_new(100, 0);
+        struct Node* n0 = node_new_leaf(__CONST_FLOAT, v0);
+
+        struct Complex* v1 = complex_new(200, 0);
+        struct Node* n1 = node_new_leaf(__CONST_FLOAT, v1);
+
+        struct Complex* v2 = complex_new(1, 0);
+        struct Node* n2 = node_new_leaf(__CONST_FLOAT, v2);
+
+        node_link(f0, n0);
+        node_link(f0, n1);
+        node_link(f0, n2);
+        node_link(f0, n2);
+        node_link(f0, n2);
+
+        return f0;
+}
+
+void f(void)
+{
+        struct Node* n0 = create_putchar();
+        struct CalcNode cn0 = calcnode_function(n0);
+
+        struct Node* n1 = create_print();
+        struct CalcNode cn1 = calcnode_function(n1);
+
+        struct Node* n2 = create_putpixel();
+        struct CalcNode cn2 = calcnode_function(n2);
+}
+
+blMain()
+{
+        f();
+
+        bl_wait(-1);
+}
